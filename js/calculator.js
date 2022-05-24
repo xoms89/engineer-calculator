b3f36db271dfd8b82ca9a6635e1ed881f8eb9dd1
xoms89 <103363502+xoms89@users.noreply.github.com>
2022-05-24 15:02:37 +0300

    in progres

--- js/calculator.js
+++ js/calculator.js
@@ -1,17 +1,19 @@
+let input = document.querySelector('.input');
+
 function insert(num) {
-  document.form.textview.value = document.form.textview.value + num;
+  input.textContent = input.textContent + num;
 }
 
 function clean() {
-  document.form.textview.value = '';
+  input.textContent = '';
 }
 
 function back() {
-  let exp = document.form.textview.value;
-  document.form.textview.value = exp.substring(0, exp.length - 1);
+  let exp = input.textContent;
+  input.textContent = exp.substring(0, exp.length - 1);
 }
 
 function equal() {
-  const exp = document.form.textview.value;
-  document.form.textview.value = eval(exp);
+  const exp = input.textContent;
+  input.textContent = eval(exp);
 }
