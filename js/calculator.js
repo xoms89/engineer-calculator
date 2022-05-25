1aa040860eaea4fb0bd5b11eb64defc9cff09404
xoms89 <103363502+xoms89@users.noreply.github.com>
2022-05-25 15:36:40 +0300

    in prigres2

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,26 @@
 let input = document.querySelector('.input');
 
 function insert(num) {
-  input.textContent = input.textContent + num;
+  if (input.textContent == 0) {
+    input.textContent = '';
+    input.textContent = input.textContent + num;
+  } else {
+    input.textContent = input.textContent + num;
+  }
 }
 
 function clean() {
-  input.textContent = '';
+  input.textContent = '0';
 }
 
 function back() {
   let exp = input.textContent;
+
   input.textContent = exp.substring(0, exp.length - 1);
+
+  if (input.textContent == 0) {
+    input.textContent = '0';
+  }
 }
 
 function equal() {
