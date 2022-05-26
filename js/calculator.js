1f2742a47a0723d71fff21a1f28d79b9f4f258ae
xoms89 <103363502+xoms89@users.noreply.github.com>
2022-05-26 22:47:23 +0300

    Progress4

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,16 @@
 // Поле ввода
 let input = document.querySelector('.input');
 
+// Сохраненная часть до возведения в степень.
+let power = '';
+
 // Вставить символ.
 function insert(num) {
   if (input.textContent == 0) {
     input.textContent = '';
-    input.textContent = input.textContent + num;
+    input.textContent += num;
   } else {
-    input.textContent = input.textContent + num;
+    input.textContent += num;
   }
 }
 
@@ -30,7 +33,20 @@ function back() {
 // Посчитать выражение.
 function equal() {
   let exp = input.textContent;
-  input.textContent = eval(exp);
+// В случае вічисления произвольной степени
+  if (input.textContent.includes('^')) {
+    let tmp = input.textContent.split('^');
+    let num = eval(power);
+    let pow = +tmp[1]; // "+" перед значением перобразовывает его в число
+    input.textContent = Math.pow(num, pow)
+    power = '';
+    return;
+  }
+
+  if (exp) {
+    input.textContent = eval(exp);
+  }
+
 }
 
 // Для вычислеия процетов.
@@ -50,17 +66,17 @@ function constant(name) {
 
   switch (name) {
     case 'pi':
-      input.textContent = input.textContent + Math.PI.toFixed(8);
+      input.textContent += Math.PI.toFixed(8);
       break;
 
     case 'e':
-      input.textContent = input.textContent + Math.E.toFixed(8);
+      input.textContent += Math.E.toFixed(8);
       break;
 
   }
 }
 
-// Квадратный коренб, квадрат, -1 степень.
+// Квадратный коренб, квадрат, -1 степень, произвольная степень
 function operation(name) {
 
   switch (name) {
@@ -75,7 +91,34 @@ function operation(name) {
     case '^-1':
       input.textContent = Math.pow(eval(input.textContent), -1);
       break;
+
+    case '^':
+      power = input.textContent;
+      input.textContent += '^';
+      break;
     }
+}
+
+// Факториал числа.
+function factorial(n) {
+  return (n !== 1) ? n * factorial(n - 1) : 1;
+}
+function fact() {
+  input.textContent = factorial(eval(input.textContent));
+}
+
+// Логарифмы
+function log(name) {
+
+  switch (name) {
+    case 'lg':
+      input.textContent = Math.log10(eval(input.textContent));
+      break;
+
+    case 'ln':
+      input.textContent = Math.log(eval(input.textContent));
+      break;
+  }
 }
 
 
