1c68d8fc56499f246b224b60df59d2168a2fe015
xoms89 <103363502+xoms89@users.noreply.github.com>
2022-05-26 13:52:40 +0300

    Progress3

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,7 @@
+// Поле ввода
 let input = document.querySelector('.input');
 
+// Вставить символ.
 function insert(num) {
   if (input.textContent == 0) {
     input.textContent = '';
@@ -9,10 +11,12 @@ function insert(num) {
   }
 }
 
+// Очистить все поле.
 function clean() {
   input.textContent = '0';
 }
 
+// Удалить символ.
 function back() {
   let exp = input.textContent;
 
@@ -23,7 +27,55 @@ function back() {
   }
 }
 
+// Посчитать выражение.
 function equal() {
-  const exp = input.textContent;
+  let exp = input.textContent;
   input.textContent = eval(exp);
 }
+
+// Для вычислеия процетов.
+function percent() {
+  input.textContent = eval(input.textContent) / 100;
+}
+
+// Для добавления констант.
+function constant(name) {
+
+   if (input.textContent !== 0 && input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('*') || input.textContent.includes('/')) {
+     input.textContent = input.textContent;
+   } else if (input.textContent !== 0) {
+    input.textContent = '';
+   }
+
+
+  switch (name) {
+    case 'pi':
+      input.textContent = input.textContent + Math.PI.toFixed(8);
+      break;
+
+    case 'e':
+      input.textContent = input.textContent + Math.E.toFixed(8);
+      break;
+
+  }
+}
+
+// Квадратный коренб, квадрат, -1 степень.
+function operation(name) {
+
+  switch (name) {
+    case 'sqrt':
+      input.textContent = Math.sqrt(eval(input.textContent));
+      break;
+
+    case 'sqr':
+      input.textContent = Math.pow(eval(input.textContent), 2);
+      break;
+
+    case '^-1':
+      input.textContent = Math.pow(eval(input.textContent), -1);
+      break;
+    }
+}
+
+
